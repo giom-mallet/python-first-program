25a1332eb2997edb28ef7a0c50b0e4e7d8c18e0e
giom-mallet <giom.mallet@live.com>
2019-09-26 21:59:49 +0800

    Added a sentencer function for section 8

---
+++ the_basics/sentencer.py
@@ -0,0 +1,21 @@
+user_input = ""
+resultstring = []
+
+while user_input != "/end" : 
+    if user_input != "" :
+        resultstring.append(user_input)
+        print ("appending : {}".format(user_input))
+    print(resultstring)
+    user_input = input("Say Something : ")
+    
+
+final_string = ""
+for sentences in resultstring :
+    final_string = final_string + sentences.title().rstrip()
+    if final_string[-1:] == "?" or final_string[-1:] == "." or final_string[-1:] == "!":
+        continue
+    final_string = final_string + ". "
+    
+
+print (final_string)
+
