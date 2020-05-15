425664d0473f0037b6f1a3d4c828abc136bcafb0
Guillaume <giom.mallet@live.com>
2020-05-15 09:26:35 +0800

    Ajout d'un champs de texte

--- gui_program/program/first_window.py
+++ gui_program/program/first_window.py
@@ -1,8 +1,15 @@
 from tkinter import *
 
+## Instrospection in Pyth
 window = Tk()
 
 button=Button(window, text="Hello World!")
-button.grid(row=1,column=1,rowspan=2,columnspan=2)
+button.grid(row=0,column=0,rowspan=1,columnspan=1)
+
+e1=Entry(window)
+e1.grid(row=0,column=1,columnspan=2)
+
+t1=Text(window)
+t1.grid(row=1,column=0,columnspan=3)
 
 window.mainloop()
