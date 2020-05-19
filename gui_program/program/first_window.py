7d890d516e0b6463297609c12ad42f5fde9b1709
Guillaume <giom.mallet@live.com>
2020-05-19 16:48:05 +0800

    TKinter examples

--- gui_program/program/first_window.py
+++ gui_program/program/first_window.py
@@ -3,13 +3,27 @@ from tkinter import *
 ## Instrospection in Pyth
 window = Tk()
 
-button=Button(window, text="Hello World!")
+def km_to_miles() :
+    txt=""
+    try:
+        km = float(e1_value.get())
+        miles = km*1.6
+        txt="{} km is {} miles".format(km, miles)
+    except:
+       txt="You must input a number"
+
+    print(txt)
+    t1.delete(1.0, END)
+    t1.insert(END, txt)
+
+button=Button(window, text="Hello World!", command=km_to_miles)
 button.grid(row=0,column=0,rowspan=1,columnspan=1)
 
-e1=Entry(window)
+e1_value=StringVar()
+e1=Entry(window, textvariable=e1_value)
 e1.grid(row=0,column=1,columnspan=2)
 
-t1=Text(window)
+t1=Text(window, height=2, width=20)
 t1.grid(row=1,column=0,columnspan=3)
 
 window.mainloop()
